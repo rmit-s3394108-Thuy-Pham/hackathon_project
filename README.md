e7352c20fbd4e81c39fa1245c8bcdff91987d7d2
Sulz <Sulienrigby@gmail.com>
2018-08-11 10:10:17 +1000

    Added readme, gitignore, updated interface code. Initial implementation tests of sqlite included.

---
+++ README.md
@@ -0,0 +1,14 @@
+# [App Name]
+This project aims to bridge the gap for students who have trouble finding teammates.
+
+## Motivation
+This project is being developed for the RMIT ITS Hackathon.
+It is not uncommon for students to find themselves in groups which they had little control over. This often leads to unbalanced work loads, neglected aspects of the work, and provides minimal opportunity for students to grow form eath others' experience.
+The app will allow students to present their strengths/weaknesses and evaluate good teammates based on their profiles.
+
+## Authors
+ - Pham Nhu Anh Thuy
+ - Sulien Rigby
+ - Trung
+ - Ama
+ - Milindi
--- server_interface.py
+++ server_interface.py
@@ -3,14 +3,70 @@ from flask import json
 from flask import jsonify
 from flask import render_template
 from flask import request
+# from flask_sqlalchemy import SQLAlchemy
+
+import sqlite3 as sq
 
 app = Flask(__name__)
 
+DATABASE_NAME = "student_profiles"
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+# db = SQLAlchemy(app)
+
+""""""
+# class test_user(db.Model):
+    # def __init__():
+        # id = db.Column(db.Integer, primary_key=True)
+        # username = db.Column(db.String(80), unique=True, nullable=False)
+        # email = db.Column(db.String(120), unique=True, nullable=False)
+#
+    # def __repr__(self):
+         # return '<User %r>' % self.username
+
 @app.route("/")
 def home():
-    initilise()
+    conn = create_connection()
+    with conn:
+        update_student(conn, ('Thuy', '12 station', 'Melbourne', 1))
+        update_student(conn, ('Sulz', '2 lenon', 'Melbourne', 2))
+    select_all_student(conn)
+    # initilise()
     return render_template('blank_starter.html')
     return null;
+
+@app.route("/read_database")
+
+@app.route("/bb")
+def select_all_student():
+    conn = create_connection()
+    cur = conn.cursor()
+
+    # TODO!
+    #cur.execute("SELECT * FROM" + DATABASE_NAME)
+    cur.execute("SELECT * FROM student_profile7")
+    rows = cur.fetchall()
+    print('rows', rows)
+    # for row in rows:
+    #     print('Thuy')
+
+def update_student(conn, profile):
+    sql = ''' INSERT INTO students_profile7 (name, addr, city, id)
+    VALUES (?,?,?,?)'''
+    # ''' UPDATE students_profile5
+    #           SET name = ? ,
+    #               addr = ? ,
+    #               city = ?
+    #           WHERE id = ?'''
+    cur = conn.cursor()
+    cur.execute(sql, profile)
+
+def create_connection():
+    conn = sq.connect('database.db')
+    print("Database opened\n\n")
+    conn.execute('CREATE TABLE students_profile7 (name TEXT, addr TEXT, city TEXT, id TEXT)')
+    print("Table created successfully");
+    return conn;
 
 
 """On Swipe Right"""
@@ -18,7 +74,7 @@ def home():
 def accept():
     print("Someone swiped right ;) ")
     print("\n args: \n", request.args)
-    
+
     message = "Success!!"
     return jsonify(message)
 
@@ -29,7 +85,7 @@ def reject():
 
 """Initialize with random people"""
 def initilise():
-    print(user_accounts()) 
+    print(user_accounts())
     return jsonify(user_accounts())
 
     # session_users = []
@@ -43,7 +99,7 @@ def initilise():
 
     # return jsonify(session_users)
 
-    # return 
+    # return
 
 def userAttributes():
     attributes = {
@@ -55,12 +111,12 @@ def userAttributes():
 
 def user_accounts():
     users = {
-        "Tyrion Lanister" : ['7','3','7','English'],
+        "Tyrion Lanister" : ['7','3','7','English', 'tech101'],
         "Tywin Lanister" : ['2','4','1','English'],
         "Dragon Lady" : ['1','3','1','English, Dragon'],
         "Faceless Man" : ['4','9','1','English'],
 
-        
+
         "Harry Potter" : ['3','5','9','English'],
         "Hagrid" : ['4','9','1','English'],
         "Ronald" : ['8','1','5','English'],
@@ -71,4 +127,4 @@ def user_accounts():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', 5000)
+    app.run(host="0.0.0.0", port=5000)
